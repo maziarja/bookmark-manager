3297cd31150cdbd291833b80a006c08c599ad483
maziarja <maziar.ja@gmail.com>
2025-11-11 15:03:35 -0800

    Changing functionality for visiting bookmarks

--- app/bookmarks/_components/VisitBookmark.tsx
+++ app/bookmarks/_components/VisitBookmark.tsx
@@ -10,17 +10,17 @@ function VisitBookmark({
 }) {
   async function handleClickVisit() {
     await visitBookmark(bookmarkId);
+    window.open(url, "_blank", "noopener,noreferrer");
   }
 
   return (
-    <a
+    <button
       onClick={handleClickVisit}
-      href={url}
       className="flex w-full items-center gap-2 text-sm font-semibold"
     >
       <ExternalLinkIcon size={16} />
       Visit
-    </a>
+    </button>
   );
 }
 
